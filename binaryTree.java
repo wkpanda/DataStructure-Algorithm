2e393d4aeacfb0db69f3521212fb9143e0b1b93a
wang <yyywk@outlook.com>
2020-02-14 10:37:48 +0800

    二叉树和二叉堆的相关代码

---
+++ binaryTree.java
@@ -0,0 +1,131 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import com.oracle.webservices.internal.api.EnvelopeStyle.Style;
+
+public class binaryTree {
+    /**
+     * 二叉树节点
+     */
+    private static class TreeNode {
+        int data;
+        TreeNode leftChild;
+        TreeNode rightChild;
+
+        TreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+    /**
+     * 构建二叉树
+     * 
+     * @param inputList 输入序列
+     */
+    public static TreeNode createBinaryTree(LinkedList<Integer> inputList) {
+        TreeNode node = null;
+        if (inputList == null || inputList.isEmpty()) {
+            return null;
+        }
+        Integer data = inputList.removeFirst();
+        if (data != null) {
+            node = new TreeNode(data);
+            node.leftChild = createBinaryTree(inputList);
+            node.rightChild = createBinaryTree(inputList);
+        }
+        return node;
+    }
+
+    /**
+     * 二叉树前序遍历
+     * @param node  二叉树节点
+     */
+    public static void preOrderTraveral(TreeNode node){
+        if(node==null){
+            return;
+        }
+        System.out.println(node.data);
+        preOrderTraveral(node.leftChild);
+        preOrderTraveral(node.rightChild);
+    }
+
+    /**
+     * 二叉树中序遍历
+     * @param node 二叉树节点
+     */
+    public static void inOrderTraveral(TreeNode node){
+        if(node==null){
+            return;
+        }
+        inOrderTraveral(node.leftChild);
+        System.out.println(node.data);
+        inOrderTraveral(node.rightChild);
+    }
+
+    /**
+     * 二叉树后序遍历
+     * @param node  二叉树节点
+     */
+    public static void postOrderTraveral(TreeNode node) {
+        if(node==null){
+            return;
+        }
+        postOrderTraveral(node.leftChild);
+        postOrderTraveral(node.rightChild);
+        System.out.println(node.data);
+    }
+
+
+    /**
+     * 二叉树非递归前序遍历
+     * 
+     * @param root 二叉树根节点
+     */
+    public static void preOrderTRaveralWithStack(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode treeNode = root;
+        while(treeNode!=null || !stack.isEmpty()){
+            //迭代访问节点的左孩子，并入栈
+            while(treeNode!=null){
+                System.out.println(treeNode.data);
+                stack.push(treeNode);
+                treeNode = treeNode.leftChild;
+            }
+            //如果节点没有左孩子，则弹出栈顶节点，访问节点右孩子
+            if(!stack.isEmpty()){
+                treeNode = stack.pop();
+                treeNode = treeNode.rightChild;
+            }
+        }
+    }
+    /**
+     * 二叉树层序遍历
+     * @param root 二叉树根节点
+     */
+    public static void levelOrderTraversal(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            System.out.println(node.data);
+            if(node.leftChild!=null){
+                queue.offer(node.leftChild);
+            }
+            if(node.rightChild!=null){
+                queue.offer(node.rightChild);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> inputList = new LinkedList<Integer>(Arrays.asList(new Integer[]{3,2,9,null,null,10,null,null,8,null,4}));
+        TreeNode treeNode = createBinaryTree(inputList);
+        System.out.println("前序遍历：");
+        preOrderTraveral(treeNode);
+        System.out.println("中序遍历：");
+        inOrderTraveral(treeNode);
+        System.out.println("后序遍历：");
+        postOrderTraveral(treeNode);
+    }
+}
--- datawhale_practice/day05/balanceString.cpp
+++ datawhale_practice/day05/balanceString.cpp
@@ -1,46 +1,52 @@
 #include <iostream>
 #include <algorithm>
 #include <unordered_map>
-#include<vector>
+#include <vector>
 using namespace std;
 
 class Solution
 {
 public:
-    int balanceString(string s){
-         vector<char> chars{'Q', 'W', 'E', 'R'};
-        unordered_map<char,int> cnt;
-        for(char ch:s)
+    int balanceString(string s)
+    {
+        vector<char> chars{'Q', 'W', 'E', 'R'};
+        unordered_map<char, int> cnt;
+        for (char ch : s)
             ++cnt[ch];
-        int expection=s.size()/4;   //每个字母期望出现的次数
-        bool balance=true;
-        for(char ch:chars){
-            if(cnt[ch]!=expection)
-                balance=false;
-            cnt[ch]-=expection; 
+        int expection = s.size() / 4; //每个字母期望出现的次数
+        bool balance = true;
+        for (char ch : chars)
+        {
+            if (cnt[ch] != expection)
+                balance = false;
+            cnt[ch] -= expection;
         }
-        if(balance==true)           //不用替换就已经平衡
+        if (balance == true) //不用替换就已经平衡
             return 0;
-        
-        int left=0,right=0,n=s.size(),ans=n;    //滑动窗口，用左右两个指针
-        while(left<=right&&right<n){
+
+        int left = 0, right = 0, n = s.size(), ans = n; //滑动窗口，用左右两个指针
+        while (left <= right && right < n)
+        {
             --cnt[s[right]];
-            bool find=true;         //find表示是否找到可以替换的子字符串
-            while(find){            //当找到可以替换的子字符串时，有可能该子字符串的前缀包含了一些无关替换的字符，所以循环检测，每次left指针右移一位
-                for(char ch:chars){ //判断目前是否已经找到子字符串
-                   if(cnt[ch]>0){   
-                       find=false;
-                       break;
-                   }      
-                }  
-                if(find==true){         //找到了就计算子字符串长度，找不到就继续找：right++
-                    ans=min(ans,right-left+1);
+            bool find = true; //find表示是否找到可以替换的子字符串
+            while (find)
+            { //当找到可以替换的子字符串时，有可能该子字符串的前缀包含了一些无关替换的字符，所以循环检测，每次left指针右移一位
+                for (char ch : chars)
+                { //判断目前是否已经找到子字符串
+                    if (cnt[ch] > 0)
+                    {
+                        find = false;
+                        break;
+                    }
+                }
+                if (find == true)
+                { //找到了就计算子字符串长度，找不到就继续找：right++
+                    ans = min(ans, right - left + 1);
                     ++cnt[s[left++]];
                 }
             }
             ++right;
         }
         return ans;
-
     }
 }
